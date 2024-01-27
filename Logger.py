5364c1148eaa508f00e123b26c4cd1637eb6a447
iamecorp <139988549+iamecorp@users.noreply.github.com>
2024-01-27 16:08:09 +0400

    del

--- Logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-class Logger:
-    def __init__(self):
-        logging.basicConfig(level=logging.INFO)
-        self.logger = logging.getLogger("ComplexCalculator")
-
-    def log(self, message):
-        self.logger.info(message)
