61bfce7d176d6cfc24afa391b22c589874877309
iamecorp <139988549+iamecorp@users.noreply.github.com>
2024-01-27 16:08:19 +0400

    Del

--- ComplexNumber.py
+++
@@ -1,7 +0,0 @@
-class ComplexNumber:
-    def __init__(self, real, imaginary):
-        self.real = real
-        self.imaginary = imaginary
-
-    def __str__(self):
-        return f"{self.real} + {self.imaginary}j"
